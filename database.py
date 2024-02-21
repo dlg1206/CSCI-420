21cbba312bfd0ed35fe81fe199c418a8eaae4556
Derek Garcia <dlg1206@g.rit.edu>
2024-02-20 20:58:16 -0500

    feat: add db columns to pandas dataframe conversion method

--- database.py
+++ database.py
@@ -24,16 +24,33 @@ class Database:
             print("Failed to establish connection with database!")
             exit(1)
 
-    def get_column(self, column: str, table: str) -> Series:
+    def get_column(self, table: str, column: str) -> Series:
         """
         Get column from Database and convert it into a series
 
-        :param column: Name of column to get
         :param table: Name of table to query
-        :return:
+        :param column: Name of column to get
+        :return: Series
         """
 
         with self.connection.cursor() as cursor:
             cursor.execute(f"SELECT {column} FROM {table}")
             records = cursor.fetchall()
             return pd.Series(map(lambda x: x[0], records))
+
+    def get_columns(self, table: str, columns: list[str]) -> DataFrame:
+        """
+        Get serval columns from the Database and convert it into a datafram
+
+        :param table: Name of table to query
+        :param columns: Name of columns to get
+        :return: Dataframe
+        """
+        with self.connection.cursor() as cursor:
+            columns_str = (str(columns)
+                           .replace("[", "")
+                           .replace("]", "")
+                           .replace("'", "\""))  # for postgres column
+
+            cursor.execute(f"SELECT {columns_str} FROM {table}")
+            return pd.DataFrame(cursor.fetchall(), columns=columns)
