ec58ef74e7add4adea6403bc4b2edf353aebde27
Sam McCune <sjm4593@g.rit.edu>
2024-02-27 02:52:57 -0500

    eda wrap up and updated requirements.txt

--- eda.py
+++ eda.py
@@ -1,11 +1,14 @@
 import os
 import time
 from pathlib import Path
+from random import sample
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from dotenv import load_dotenv
-
+from scipy.stats import ttest_ind, ttest_rel
+from scipy import stats
 from database import Database
 from util.LogMessage import LogMessage
 
@@ -26,27 +29,42 @@ db = Database(
 )
 
 
-def bivariateAnalysisTwoVariables(var1, var2):
+def bivariateAnalysisCountPlot(data_num, var1, var2, suffix):
     """
     This function will print the bi-variate analysis to the console and print a scatter plot showing the
     correlation between the two
+    :param: data_num: the numeric data values
     :param: var1: the first numeric attribute in the bi-variate analysis
     :param: var2: the second numeric attribute in the bi-variate analysis
+    :suffix: the table being taken from
     :return: All the summary statistics to the console
     """
-    print("do stuff")
+    plt.figure(figsize=(12, 12))
+    sns.boxplot(x=var1, y=var2, data=data_num)
+    plt.savefig((f"SavedImages/boxplot{var1}{var2}{suffix}.png").replace(" ", ""))
+    plt.clf()
 
+    data = data_num[(np.abs(stats.zscore(data_num[var2])) < 3)]
 
-def multivariateAnalysisThreeVariables(var1, var2, var3):
-    """
-    This function will print the multivariate analysis to the console and print a scatter plot showing the
-    correlation between the three
-    :param: var1: the first numeric attribute in the multivariate analysis
-    :param: var2: the second numeric attribute in the multivariate analysis
-    :param: var3: the second numeric attribute in the multivariate analysis
-    :return: All the summary statistics to the console
-    """
-    print("do stuff")
+    var1_1 = data[data[var1] > 3.0][var2]
+    var1_0 = data[data[var1] <= 3.0][var2]
+
+    var1_0 = var1_0.values.tolist()
+    var1_0 = sample(var1_0, 100)
+    var1_1 = var1_1.values.tolist()
+    var1_1 = sample(var1_1, 100)
+
+    ttest, pval = ttest_ind(var1_1, var1_0, equal_var=False)
+    print(f"{suffix}"
+          "\nNull Hypothesis :- there is no difference in Mean of review length in overall > 3.0 and overall < 3.0. "
+          "\nAlternate Hypothesis :- there is difference in Mean of review length in overall > 3.0 and overall < 3.0.\n")
+    print("ttest", ttest)
+    print('p value', format(pval, '.70f'))
+
+    if pval < 0.05:
+        print("we reject null hypothesis\n")
+    else:
+        print("we accept null hypothesis\n")
 
 
 def getHistogram(data_num, column, suffix):
@@ -57,9 +75,10 @@ def getHistogram(data_num, column, suffix):
     :param: suffix: end of the sql statement
     :return: The histogram as an image
     """
-    data_num[column].hist(figsize=(8, 8))
+    data_num[column].hist(figsize=(12, 12))
     plt.title(f"{column}: {suffix}.")
     plt.savefig((f"SavedImages/Univariate_histogram{column}{suffix}.png").replace(" ", ""))
+    plt.clf()
 
 
 def performEdaOnTable(suffix: str):
@@ -100,10 +119,22 @@ def performEdaOnTable(suffix: str):
     getHistogram(data_num, 'vote', suffix)
 
     # Show correlations on numerical data
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(12, 12))
     sns.heatmap(data_num.corr(), annot=True, linewidths=.5, cmap="Blues")
     plt.title('Heatmap showing correlations between numerical data')
     plt.savefig((f"SavedImages/Multivariate_heatmap{suffix}.png").replace(" ", ""))
+    plt.clf()
+
+    # Create countplot
+    bivariateAnalysisCountPlot(data_num, "overall", "text_lengths", suffix)
+
+    # Binning Overall and Text Lengths
+    high_values = len(data_num[data_num['overall'] > 3.0]) / len(data_num['overall'])
+    low_values = len(data_num[data_num['overall'] <= 3.0]) / len(data_num['overall'])
+    print(f"\nWe will define our high values as overall scores > 3.0"
+          "\nOur low value will be overall scores <= 3.0"
+          f"\n% in high values is {high_values}"
+          f"\n% in low values is {low_values}")
 
 
 if __name__ == '__main__':
