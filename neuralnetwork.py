e4760cb2327e0fe182af47da239ba95aeaa923a8
Sam McCune <sjm4593@g.rit.edu>
2024-04-11 11:14:40 -0400

    Update Neural Network stuff

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 import keras
 import numpy as np
 import pandas as pd
@@ -46,14 +47,13 @@ def GetAllData(sql_statement):
     # Get the numerical values in the dataframe
     new_df = df.select_dtypes(include=np.number)
     new_df = new_df.drop(['vote', 'unixreviewtime'], axis=1)
-    new_df = new_df.drop([9, 10], axis=1)
     new_df.columns = new_df.columns.astype(str)
-    new_df.rename(columns={'11': "score"})
+    new_df = new_df.drop(['9', '10', '12'], axis=1)
 
     # Add a new column to the data frame that has the review text length
     new_df.insert(3, 'text_lengths', text_lengths)
+    new_df.columns = ["overall", "uid", "text_lengths", "score"]
     print(new_df)
-    print("hi")
 
     # Scale the dataframe
     scaler = preprocessing.MinMaxScaler()
@@ -78,17 +78,46 @@ def RunNeuralNet(table_name):
     train, test = GetAllData(table_name)
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(4, activation='relu', input_shape=(3,)),
+        tf.keras.layers.Dense(4, activation='relu', input_shape=(2,)),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(4, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit(train['overall'], train['text_length'], train['score'],  epochs=1000)
-
-    test_input = test
+    test_input = test[['score', 'text_lengths']]
+    history = model.fit(train[['score', 'text_lengths']].to_numpy(), train[['overall']].to_numpy(), epochs=100)
     test_output = model.predict(test_input)
-    print(test_output)
+
+    # Accuracy graphs
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    plt.savefig("neuralnet.pdf")
+
+    # Print example outputs
+    print("\n")
+    total = 0
+    overall_array = test[['overall']].to_numpy()
+    for i in range(0, len(test_output)):
+        # Calculate predicted output
+        overall = math.ceil((test_output[i][0] * 5))
+
+        if i < 100:
+            print(f"{test[['score', 'text_lengths', 'overall']].iloc[i]} "
+                  f"\nPredicted Output: {overall}\n")
+
+        # Calculate the accuracy
+        if overall == math.ceil(overall_array[i][0] * 5):
+            total = total + 1
+
+    # Evaluate the model
+    print(f"\n\nAccuracy on Test Data As Classification Problem: {total/len(test_output)}")
 
 
 if __name__ == '__main__':
